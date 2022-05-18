9924f14743d48c3f3b30c5185380431cd4bb5281
Captain Sev <captain.sev@outlook.com>
2022-05-18 18:41:01 +0200

    Fixed notifications overflow on small devices

---
+++ js/button.js
@@ -0,0 +1,14 @@
+const Button = {
+    startWaiting(button) {
+        button.classList.add("waiting")
+    },
+    stopWaiting(button) {
+        button.classList.remove("waiting")
+    },
+    waitForPromise(button, promise) {
+        this.startWaiting(button)
+        promise.finally(() => {
+            this.stopWaiting(button)
+        })
+    }
+}
---
+++ js/notifications.js
@@ -0,0 +1,31 @@
+var Notification = {
+    Type: {
+        INFO: "info",
+        SUCCESS: "succes",
+        WARN: "warn",
+        ERROR: "error",
+    },
+    notify(type, message, duration=3000) {
+        let notification = document.createElement("div")
+        notification.className = "notification hidden"
+        let p = document.createElement("p")
+        p.className = "message"
+        notification.appendChild(p)
+        p.innerText = message
+        switch (type) {
+            case this.Type.INFO:
+            case this.Type.SUCCESS:
+            case this.Type.WARN:
+            case this.Type.ERROR:
+                notification.classList.add(type)
+            default:
+                console.warn(`Provided type '${type}' does not exist.`)
+        }
+
+        notifications.appendChild(notification)
+        setTimeout(() => { notification.classList.remove("hidden") }, 10)
+        setTimeout(() => { notification.classList.add("hidden") }, duration + 10)
+        setTimeout(() => { notification.classList.add("removing") }, duration + 510)
+        setTimeout(() => { notification.remove() }, duration +1010)
+    }
+}
