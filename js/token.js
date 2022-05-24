9699671d86df1b320e9eb70a6cb07a3fbddf4a48
Captain Sev <captain.sev@outlook.com>
2022-05-24 14:18:27 +0200

    Added token js

---
+++ js/token.js
@@ -0,0 +1,16 @@
+if (!window.ethers) {
+    throw new Error("Require etherjs in the global scope to work!")
+}
+
+const BigNumber = ethers.BigNumber
+
+function decimalFromWei(bn, decimals, precision = 4) {
+    // If the decimals is just 2 then a precision of 4 doesn't make sense 
+    // and leads to errors in the calculation.
+    const correctedPrecision = Math.min(precision, decimals)
+
+    // We create a cutoff at the offsetted decimal position.
+    const cutoffPosition = decimals-correctedPrecision
+    const precisionNumber = bn.div(BigNumber.from("10").pow(cutoffPosition))
+    return precisionNumber.toNumber() / Math.pow(10, correctedPrecision)
+}
